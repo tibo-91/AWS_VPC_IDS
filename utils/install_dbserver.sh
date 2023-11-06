20984af6e157a1e237ef7242a3986fbcabbf62ff
tibo-91 <thibault.renou@proton.me>
2023-11-06 15:16:49 +0100

    fix sqlilab

--- utils/install_dbserver.sh
+++ utils/install_dbserver.sh
@@ -5,7 +5,7 @@ sudo yum update -y
 sudo yum upgrade -y
 sudo yum install mariadb105-server -y
 sudo systemctl start mariadb;
-sudo mysql --execute "CREATE USER 'aws' IDENTIFIED BY 'pass';"
+sudo mysql --execute "CREATE USER 'aws' IDENTIFIED BY 'pass'; GRANT ALL PRIVILEGES ON * . * TO 'aws'; FLUSH PRIVILEGES;"
 echo "skip-networking=0" >> /etc/my.cnf.d/mariadb-server.cnf
 echo "skip-bind-address" >> /etc/my.cnf.d/mariadb-server.cnf
 sudo systemctl restart mariadb

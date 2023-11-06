40a59f11f8c35ca6f67455c1b157f4483f79fcda
tibo-91 <thibault.renou@proton.me>
2023-11-06 15:32:53 +0100

    test IDS

--- utils/install_webserver.sh
+++ utils/install_webserver.sh
@@ -40,9 +40,12 @@ sed -i s/\$dbpass\ \=\'\'/\$dbpass\ \=\'pass\'/g /var/www/html/sqli/sql-connecti
 sudo systemctl restart apache2
 
 
-# install and configure snort
-#sudo apt install snort -y
+# Install Snort
+sudo apt install snort -y
 
 # Configure Snort to log SQL injection alerts on port 80
-#echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"select"; sid:1000001;)' | sudo tee -a /etc/snort/rules/local.rules
-#sudo service snort restart
+echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"select"; sid:1000001;)' | sudo tee -a /etc/snort/rules/local.rules
+echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"SELECT"; sid:1000002;)' | sudo tee -a /etc/snort/rules/local.rules
+echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"%27"; sid:1000003;)' | sudo tee -a /etc/snort/rules/local.rules # %27 is '
+echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"%22"; sid:1000004;)' | sudo tee -a /etc/snort/rules/local.rules # %22 is "
+sudo service snort restart

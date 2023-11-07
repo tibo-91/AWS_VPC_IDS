f6cd22ebee2dcfd68b6a38c2cb53c7f48b6a0757
tibo-91 <thibault.renou@proton.me>
2023-11-07 22:38:03 +0100

    test seb5

--- utils/install_ids.sh
+++ utils/install_ids.sh
@@ -119,7 +119,7 @@ while true; do
             "ssh -i ~/.ssh/$keyname -t ubuntu@$ids_ipv4 \
                 'wget $repository_path/utils/configure_ids.sh; \
                 sudo chmod +x ./configure_ids.sh; \
-                sudo bash ./configure_ids.sh -k $network_interface_ids_id'"
+                sudo bash ./configure_ids.sh -n $network_interface_ids_id'"
 		break
 	fi
     sleep 10

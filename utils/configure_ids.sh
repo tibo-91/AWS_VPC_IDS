f4444a98657258d554272651a7529d53d96fc036
tibo-91 <thibault.renou@proton.me>
2023-11-07 22:12:36 +0100

    VERSION NORMALEMENT FINALE

--- utils/configure_ids.sh
+++ utils/configure_ids.sh
@@ -7,14 +7,12 @@ do
 		k) network_interface_ids_id=${OPTARG};;
 	esac
 done
-echo -e "\n\n\n$network_interface_ids_id\n\n\n"
 
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install snort -y
 
-# specify the interface to listen on
-# <here>
+sudo sed -i s/eth0/$network_interface_ids_id/g /etc/systemd/system/snort.service
 
 echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"select"; sid:1000001;)' | sudo tee -a /etc/snort/rules/local.rules
 echo 'alert tcp any any -> $HOME_NET 80 (msg:"SQL Injection attempt"; content:"SELECT"; sid:1000002;)' | sudo tee -a /etc/snort/rules/local.rules
